a1df2ba5c1156afe7d1e54c1c50e584dd13116b1
bryle99 <brylebaran@gmail.com>
2019-02-17 23:11:15 +0800

    all

---
+++ WriteFiles_Console/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WriteFiles_Dll;
+
+namespace WriteFiles_Console
+{
+    class Program
+    {   
+        static void Main(string[] args)
+        {
+            MyClass obj = new MyClass();
+            Console.WriteLine("Current Directory of DLL: " + obj.GetParentDirectory());
+            obj.DisplayDirectory();
+            obj.WriteFiles();
+            Console.Read();
+        }
+    }
+}
---
+++ WriteFiles_Dll/Class1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+
+namespace WriteFiles_Dll
+{
+    public class MyClass
+    {
+
+        public string GetParentDirectory() {
+            Type myType = typeof(MyClass);
+            string directory;
+            directory = Directory.GetParent(Directory.GetCurrentDirectory()).ToString();
+            directory = Directory.GetParent(directory).ToString();
+            directory = Directory.GetParent(directory).ToString();
+            directory += "\\" + myType.Namespace;
+
+            return directory;
+        }
+
+        public string GetDirectory() {
+            //string[] files = Directory.GetFiles(ReturnCurrentDirectory());
+            //string[] files = Directory.GetDirectories(@"C:\Users\wabzs\source\repos\", "WriteFiles_Dll.dll", SearchOption.AllDirectories);
+            //string[] files = Directory.GetFiles(@"C:\Users\wabzs\source\repos\WriteFiles_Dll\bin\Debug\netstandard2.0");
+            string[] dirs = Directory.GetFiles(GetParentDirectory(), "*.dll", SearchOption.AllDirectories);
+            string ret = "";
+            foreach (string dir in dirs) {
+                if (dir.Contains("bin")) {
+                    ret = dir;
+                    ret = Directory.GetParent(ret).ToString();
+                }
+            }
+            //Type myType = typeof(MyClass);
+
+            return ret;
+        }
+
+        public void DisplayDirectory() {
+            Console.WriteLine(GetDirectory());
+        }
+
+        public void WriteFiles() {
+            string[] files = Directory.GetFiles(GetDirectory(), "*.txt", SearchOption.AllDirectories);
+            string message = "This has been written by a C# dll";
+            foreach (string file in files) {
+                Console.WriteLine(file + "  Has been written!");
+                File.WriteAllText(file, message);
+            }
+        }
+    }
+}
